90fa88a9968895501316c88e9213116f801a1fb0
Yogendra singh <yogendra.singh@rebelle.de>
2023-02-26 14:30:36 +0530

    Changes for API and error boundry

--- src/services/utils/axios.ts
+++ src/services/utils/axios.ts
@@ -53,7 +53,7 @@ const errorHandler = (error: any) => {
     });
   }
 
-  Sentry.captureException("sport eco error", error.response.message);
+  Sentry.captureException("sport-eco API error", error.response);
   throw error;
 };
 
